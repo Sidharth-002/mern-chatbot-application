979d1988d1a4b2f680da2667f72d249f0779addc
Sidharth <sidharth.kondabattini@conduira.com>
2026-08-10 18:03:45 +0530

    Cors error fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
-app.use(cors());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(express.json());
 
 mongoose
